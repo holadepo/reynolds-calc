90d9eb2d9fcc89c0cee87d4921957f664520435b
Habibullah Oladepo <habibullaho@yahoo.com>
2018-05-17 22:25:11 +0100

    Function to get variables then print reynolds no

--- reynolds_calc.py
+++ reynolds_calc.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+def main():
+    # get density, flow velocity, length and viscosity from user,
+    # then calculte Reynolds number
+
+    density = float(input('fluid density: '))
+    velocity = float(input('flow velocity: '))
+    length = float(input('length: '))
+    viscosity = float(input('fluid viscosity: '))
+
+    reynolds_no = (density * velocity * length) / viscosity
+    print('Renynolds number = {0}'.format(reynolds_no))
+
+    return
+
+main()
