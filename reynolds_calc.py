4913c9c92b4fb398dc7270891e352c0427392da4
Habibullah Oladepo <habibullaho@yahoo.com>
2018-05-20 11:13:29 +0100

    Change to notify user of units when seeking input

--- reynolds_calc.py
+++ reynolds_calc.py
@@ -4,10 +4,10 @@ def main():
     # get density, flow velocity, length and viscosity from user,
     # then calculte Reynolds number
 
-    density = float(input('fluid density: '))
-    velocity = float(input('flow velocity: '))
-    length = float(input('length: '))
-    viscosity = float(input('fluid viscosity: '))
+    density = float(input('fluid density [kg/m^3] : '))
+    velocity = float(input('flow velocity [m/s] : '))
+    length = float(input('length [m] : '))
+    viscosity = float(input('fluid viscosity [kg/(m.s)] : '))
 
     reynolds_no = (density * velocity * length) / viscosity
     print('Renynolds number = {0}'.format(reynolds_no))
