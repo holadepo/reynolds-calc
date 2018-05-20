314dc72370a62dededfecfca769ffd275956f023
Habibullah Oladepo <habibullaho@yahoo.com>
2018-05-20 16:04:58 +0100

    combined input functions into one

--- reynolds_calc.py
+++ reynolds_calc.py
@@ -1,46 +1,54 @@
 #!/usr/bin/env python3
+import sys
 
-def getDensity():
-    """
-    Get fluid density from user
-    """
-    density = float(input('fluid density [kg/m^3] : '))
-    return density
+def getInput(variable):
+    '''
+    Get input variables from user
+    INPUT:
+        variable (string): the name of the variable to get. This
+            can be 'density', 'velocity', 'length', 'dynViscosity',
+            'kinViscosity', or 'reynoldsNo'.
+    OUTPUT:
+        inputValue (float): the value entered by the user
+    '''
+    message =   {
+                    'density'       :'fluid density [kg/m^3] : ',
+                    'velocity'      :'flow velocity [m/s] : ',
+                    'length'        :'length [m] : ',
+                    'dynViscosity'  :'fluid dynamic viscosity [kg/(m.s)] : ',
+                    'kinViscosity'  :'fluid kinematic viscosity [m^2/s] : ',
+                    'reynoldsNo'    :'flow Reynolds number'
+                }
 
-def getVelocity():
-    """
-    Get free stream flow velocity from user
-    """
-    velocity = float(input('flow velocity [m/s] : '))
-    return velocity
-
-def getLength():
-    """
-    Get length from user
-    """
-    length = float(input('length [m] : '))
-    return length
-
-def getDynViscosity():
-    """
-    Get fluid dynamic viscosity from user
-    """
-    dynViscosity = float(input('fluid dynamic viscosity [kg/(m.s)] : '))
-    return dynViscosity
+    inputValue = None
+    try:
+        inputValue = float(input(message[variable]))
+    except KeyError:
+        print('variable \'{0}\' not recognised'.format(variable))
+        print('Program terminated\n')
+        sys.exit()
+    return inputValue
 
 def main():
     """
     Main program logic
     """
 
-    density = getDensity()
-    velocity = getVelocity()
-    length = getLength()
-    dynViscosity = getDynViscosity()
+    # spacing: for clarity
+    print('')
+    
+    # inputs
+    density = getInput('density')
+    velocity = getInput('velocity')
+    length = getInput('velocity')
+    dynViscosity = getInput('dynViscosity')
 
     reynoldsNo = (density * velocity * length) / dynViscosity
     print('Renynolds number = {0}'.format(reynoldsNo))
-
+   
+   # spacing: for clarity
+    print('')
+    
     return
 
 main()
